5d629d1ee77d62ead28fdc775a816685a4dd1848
DimaBulgakov <bulka20072@gmail.com>
2025-10-09 12:25:50 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,17 +1,7 @@
-"""Модуль для работы с текстовыми сообщениями"""
-
-def get_message(name: str = "World") -> str:
-    """
-    Генерирует приветственное сообщение.
-
-    Args:
-        name: Имя для приветствия.
-
-    Returns:
-        Строка приветствия.
-    """
-    return f"Hello, {name}!"
+"""Пояснение работы"""
 
 
-if __name__ == "__main__":
-    print(get_message())
+def text():
+    """Функция выводит сообщение message"""
+    message = "message"
+    return message
