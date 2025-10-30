80291a8d8d0d0213b27334235a15d91b25fabab4
DimaBulgakov <bulka20072@gmail.com>
2025-10-30 13:05:37 +0300

    Реализованы функции decrypt и encrypt (caesar)

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,3 +1,6 @@
+"""ШИФР ЦЕЗАРЯ"""
+
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -11,13 +14,25 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if char.isalpha():
+            if char.isupper():
+                base = ord("A")
+            else:
+                base = ord("a")
+
+            shifted_char = chr((ord(char) - base + shift) % 26 + base)
+            ciphertext += shifted_char
+        else:
+            ciphertext += char
+
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +43,16 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():
+                base = ord("A")
+            else:
+                base = ord("a")
+
+            shifted_char = chr((ord(char) - base - shift) % 26 + base)
+            plaintext += shifted_char
+        else:
+            plaintext += char
+
     return plaintext
