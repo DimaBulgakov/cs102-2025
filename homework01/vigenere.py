4d8da232a89bf7fcc5937dd8ebe9b718e555435a
DimaBulgakov <bulka20072@gmail.com>
2025-10-30 13:13:46 +0300

    Реализованы функции decrypt и encrypt (vigenere)

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+"""ШИФР ВИНЕЖЕРА"""
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +12,26 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    indx = 0
+    extended_key = ""
+    for i in range(0, len(plaintext)):
+        extended_key += keyword[i % len(keyword)]
+
+    for char in plaintext:
+        if char.isalpha():
+            shift = ord(extended_key[indx]) - ord('A')
+
+            if char.isupper():
+                encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+
+            ciphertext += encrypted_char
+            indx += 1
+        else:
+            ciphertext += char
+
     return ciphertext
 
 
@@ -24,5 +46,24 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    extended_key = ""
+    for i in range(0, len(ciphertext)):
+        extended_key += keyword[i % len(keyword)]
+    indx = 0
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(extended_key[indx]) - ord('A')
+
+            if char.isupper():
+                decrypted_char = chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                decrypted_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+
+            plaintext += decrypted_char
+            indx += 1
+        else:
+            plaintext += char
+
     return plaintext
+
